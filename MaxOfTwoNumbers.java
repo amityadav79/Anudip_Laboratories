e27ed62dc9cc7ece3e76ec7f5f992c896f082fb7
Amit Yadav <73068711+amityadav79@users.noreply.github.com>
2024-06-04 17:40:26 +0530

    Update new code
    Add some oops concept in this code

--- MaxOfTwoNumbers.java
+++ MaxOfTwoNumbers.java
@@ -1,21 +1,48 @@
 import java.util.Scanner;
 
-public class MaxOfTwoNumbers {
-    public static void main(String[] args) {
-        // Create a Scanner object to read input
-        Scanner scanner = new Scanner(System.in);
+class MaxOfTwoNumbers {
+	private int num1,num2;
+	
+	public MaxOfTwoNumbers(){
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter two number: ");
+		num1= sc.nextInt();
+       	num2= sc.nextInt();
+		System.out.println("--------------------");
+	}
 
-        System.out.print("Enter the first number: ");
-        int num1 = scanner.nextInt();
-
-        System.out.print("Enter the second number: ");
-        int num2 = scanner.nextInt();
-
-        // Use the ternary operator to find the maximum of the two numbers
+ 	public void FindMaxNum(){
+ 	// Use the ternary operator to find the maximum of the two numbers
         int max = (num1 > num2) ? num1 : num2;
-
-        // Output the maximum number
+	// Output the maximum number
         System.out.println("The maximum of the two numbers is: " + max);
-
-            }
+   }
 }
+
+class MaxNumber{
+	public static void main(String a[]){
+		MaxOfTwoNumbers m= new MaxOfTwoNumbers();
+		m.FindMaxNum();
+	  }
+	}
+		
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
