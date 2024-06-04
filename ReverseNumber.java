947c57722f63e3240aa21d4d13443e17a7b8e451
Amit Yadav <73068711+amityadav79@users.noreply.github.com>
2024-06-04 17:42:52 +0530

    Reverse Number

---
+++ ReverseNumber.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+class Reverse{
+    private int num, rev=0;
+
+    public Reverse(){
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number: ");
+        num=sc.nextInt();
+        System.out.println("-------------------------");
+    }
+
+    public void ReverseNum(){
+        while (num > 0) { 
+            int rem = num % 10; 
+            rev = (rev * 10) + rem; 
+            num = num / 10; 
+        } 
+        System.out.println("Reversed Number is: "+rev);
+
+    }
+
+}
+class ReverseNumber{
+    public static void main(String[] args) {
+        new Reverse().ReverseNum();
+     }
+}
